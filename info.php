5feb526c6e239952484443d35dccb69f6efab618
Dmitriy <dima72vas@gmail.com>
2024-03-08 00:40:34 +0700

    nginx configuration

--- info.php
+++ info.php
@@ -1,10 +1,4 @@
 <?php
-
     // Show all information, defaults to INFO_ALL
     phpinfo();
-
-    // Show just the module information.
-    // phpinfo(8) yields identical results.
-    phpinfo(INFO_MODULES);
-
 ?>
